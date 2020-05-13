2879dfa2481b4001b6a7c40d9b998cc7460209de
lununglee <leo.lunung.lee@gmail.com>
2020-05-13 14:28:57 -0700

    initial commit w/ selfmade cube

---
+++ Assets/Scripts/EditorSnap.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour
+{
+	void Update()
+	{
+		Vector3 SnapPos;
+		SnapPos.x = Mathf.RoundToInt(transform.position.x / 10.0f) * 10.0f;
+		SnapPos.z = Mathf.RoundToInt(transform.position.z / 10.0f) * 10.0f;
+		transform.position = new Vector3(SnapPos.x, 0.0f, SnapPos.z);
+	}
+}
