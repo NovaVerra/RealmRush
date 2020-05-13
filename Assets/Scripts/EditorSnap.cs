5d0e15db884dc0520c8d1f7cf604fbfee37f78a3
lununglee <leo.lunung.lee@gmail.com>
2020-05-13 14:30:10 -0700

    overwrote editor's snap to grid setting

--- Assets/Scripts/EditorSnap.cs
+++ Assets/Scripts/EditorSnap.cs
@@ -7,7 +7,9 @@ public class EditorSnap : MonoBehaviour
 {
 	void Update()
 	{
+		// Overwrites editors snap to grid setting
 		Vector3 SnapPos;
+
 		SnapPos.x = Mathf.RoundToInt(transform.position.x / 10.0f) * 10.0f;
 		SnapPos.z = Mathf.RoundToInt(transform.position.z / 10.0f) * 10.0f;
 		transform.position = new Vector3(SnapPos.x, 0.0f, SnapPos.z);
