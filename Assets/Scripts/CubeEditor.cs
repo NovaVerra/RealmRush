ccdc818b916eb4ea04d182b36cef130bcdb3de4e
lununglee <leo.lunung.lee@gmail.com>
2020-05-16 18:46:53 -0700

    display grid using text mesh

---
+++ Assets/Scripts/CubeEditor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
+{
+	Vector3		SnapPos;
+	TextMesh	TextMeshCoordinate;
+
+	[SerializeField][Range(1.0f, 20.0f)]
+	int		GridSize;
+
+	void Start()
+	{
+		TextMeshCoordinate = GetComponentInChildren<TextMesh>();
+	}
+
+	// Overwrites editors snap to grid setting
+	void Update()
+	{
+		SnapPos.x = Mathf.RoundToInt(transform.position.x / 10.0f) * GridSize;
+		SnapPos.z = Mathf.RoundToInt(transform.position.z / 10.0f) * GridSize;
+		transform.position = new Vector3(SnapPos.x, 0.0f, SnapPos.z);
+
+		TextMeshCoordinate.text = SnapPos.x / GridSize + ", " + SnapPos.z / GridSize;
+	}
+}
