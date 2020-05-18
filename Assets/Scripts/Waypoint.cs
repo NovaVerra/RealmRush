e31743f98d94c39706e455c6b96a71c4acb87e6f
lununglee <leo.lunung.lee@gmail.com>
2020-05-18 11:54:49 -0700

    renamed Block to Waypoint

--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -2,17 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyMovement : MonoBehaviour
+public class Waypoint : MonoBehaviour
 {
-	[SerializeField] List<Waypoint>	Path;
-
 	// Start is called before the first frame update
 	void Start()
 	{
-		foreach (Waypoint Coordinate in Path)
-		{
-			print(Coordinate);
-		}
+		
 	}
 
 	// Update is called once per frame
