b6e23789bfc29e491737a4e92ddcb732b5933dc0
lununglee <leo.lunung.lee@gmail.com>
2020-05-20 18:54:32 -0700

    implemented queue and setup game state with algorithm state

--- Assets/Scripts/PathFinder.cs
+++ Assets/Scripts/PathFinder.cs
@@ -5,22 +5,28 @@ using UnityEngine;
 public class PathFinder : MonoBehaviour
 {
 	/** Game State */
-	Dictionary<Vector2Int, Waypoint> WorldGrid = new Dictionary<Vector2Int, Waypoint>();
-	[SerializeField]	Waypoint StartBlock, EndBlock;
+	bool	b_IsRunning = true;
+
+	/** Game Config */
+	Dictionary<Vector2Int, Waypoint>	WorldGrid = new Dictionary<Vector2Int, Waypoint>();
+	Queue<Waypoint> 					Queue = new Queue<Waypoint>();
+	[SerializeField] Waypoint 			StartWaypoint, EndWaypoint;
+
 	Vector2Int[] Directions = {
 		Vector2Int.up,
+		Vector2Int.right,
 		Vector2Int.down,
-		Vector2Int.left,
-		Vector2Int.right
+		Vector2Int.left
 	};
 
 	// Start is called before the first frame update
 	void	Start()
 	{
 		LoadBlocks();
-		StartBlock.SetTopColor(Color.yellow);
-		EndBlock.SetTopColor(Color.red);
-		ExploreNeighbours();
+		StartWaypoint.SetTopColor(Color.yellow);
+		EndWaypoint.SetTopColor(Color.red);
+		// ExploreNeighbours();
+		Pathfind();
 	}
 
 	void	LoadBlocks()
@@ -45,11 +51,43 @@ public class PathFinder : MonoBehaviour
 	{
 		foreach (Vector2Int Direction in Directions)
 		{
-			Vector2Int NeighbourCoordinate = StartBlock.GetGridPos() + Direction;
+			Vector2Int NeighbourCoordinate = StartWaypoint.GetGridPos() + Direction;
 			if (WorldGrid.ContainsKey(NeighbourCoordinate))
 			{
 				WorldGrid[NeighbourCoordinate].SetTopColor(Color.blue);
 			}
+			// try
+			// {
+			// 	WorldGrid[NeighbourCoordinate].SetTopColor(Color.blue);
+			// }
+			// catch
+			// {
+				// ;
+			// }
+		}
+	}
+
+	void	Pathfind()
+	{
+		Queue.Enqueue(StartWaypoint);
+		while (Queue.Count > 0)
+		{
+			var SearchCenter = Queue.Dequeue();
+			print("Searching from: " + SearchCenter);
+			if (EndFound(SearchCenter)) { return ; }
+		}
+	}
+
+	bool	EndFound(Waypoint SearchCenter)
+	{
+		if (SearchCenter == EndWaypoint)
+		{
+			b_IsRunning = false;
+			return true;
+		}
+		else
+		{
+			return false;
 		}
 	}
 }
