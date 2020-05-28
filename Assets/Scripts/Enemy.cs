6ee92343365d389324a0074ab9db5e77288a98fc
lununglee <leo.lunung.lee@gmail.com>
2020-05-28 12:43:41 -0700

    implemented hitpoints and destroy on 0:

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+	[SerializeField] int	HitPoint = 25;
+
 	void	OnParticleCollision(GameObject CollisionObject)
 	{
-		print("Hit!!!");
+		Hit();
+	}
+
+	void	Hit()
+	{
+		if (HitPoint > 0)
+		{
+			HitPoint--;
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+		print("HP Remaining: " + HitPoint);
 	}
 }
