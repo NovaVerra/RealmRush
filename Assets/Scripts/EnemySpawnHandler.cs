8ace3c7ed30df4ddda6c12480e868f09ec208070
lununglee <leo.lunung.lee@gmail.com>
2020-05-29 13:42:03 -0700

    implemented an enemy spawn handler with coroutine spawning

---
+++ Assets/Scripts/EnemySpawnHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnHandler : MonoBehaviour
+{
+	[SerializeField] GameObject	EnemyPrefab;
+	[SerializeField] Transform	Parent;
+	[SerializeField] float		SpawnRate = 1.0f;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		StartCoroutine(SpawnEnemy());
+	}
+
+	IEnumerator	SpawnEnemy()
+	{
+		while (true)
+		{
+			GameObject EnemyInstance = Instantiate(EnemyPrefab);
+			EnemyInstance.transform.parent = Parent;
+			yield return new WaitForSeconds(SpawnRate);
+		}
+	}
+}
